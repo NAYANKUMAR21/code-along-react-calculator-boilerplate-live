931116a32f2a269866ebcbd0bf6b1e2026ab8590
Nayan Kumar <naayaankumar@gmail.com>
2024-02-06 15:50:27 +0530

    updated

--- calc/src/App.jsx
+++ calc/src/App.jsx
@@ -1,35 +1,140 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [allBtnClicked, setAllbtnClicked] = useState('');
+
+  const handleclick = (event) => {
+    console.log(event.target.value);
+    let strBtn = event.target.value;
+    let newSettingString = allBtnClicked + strBtn;
+
+    let symbols = '+-/*.';
+    for (let i = 0; i < newSettingString.length; i++) {
+      if (
+        symbols.includes(newSettingString[i]) &&
+        symbols.includes(newSettingString[i + 1])
+      ) {
+        console.log('inside if');
+        newSettingString = newSettingString.slice(0, -1);
+        alert('No two symbols can be together');
+      }
+    }
+
+    let exampleNum = newSettingString;
+    let numString = '123456789';
+    for (let i = 0; i < exampleNum.length; i++) {
+      if (
+        !symbols.includes(exampleNum[i]) &&
+        !symbols.includes(exampleNum[i + 1])
+      ) {
+        if (
+          (exampleNum[i] == '0' && numString.includes(exampleNum[i + 1])) ||
+          exampleNum[i + 1] == '0'
+        ) {
+          let add = '';
+          let flag = false;
+          for (let j = 0; j < exampleNum.length; j++) {
+            if (j !== i) {
+              add += exampleNum[j];
+              flag = true;
+            }
+          }
+
+          if (flag) {
+            exampleNum = add;
+            break;
+          }
+        }
+      }
+    }
+    setAllbtnClicked(exampleNum);
+  };
+
+  const handleClickDelete = () => {
+    let newStr = allBtnClicked.slice(0, -1);
+    console.log(newStr);
+    setAllbtnClicked(newStr);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="container">
+      <div className="calc-display">{allBtnClicked}</div>
+      <div className="calc-btn">
+        <button value="ac" onClick={() => setAllbtnClicked('')}>
+          AC
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <button
+          onClick={() => {
+            handleClickDelete();
+            console.log('delete last number');
+          }}
+          value="del"
+        >
+          Del
+        </button>
+        <button onClick={handleclick} value="+">
+          +
+        </button>
+        <button onClick={handleclick} value="1">
+          1
+        </button>
+        <button onClick={handleclick} value="2">
+          2
+        </button>
+        <button onClick={handleclick} value="3">
+          3
+        </button>
+        <button onClick={handleclick} value="4">
+          4
+        </button>
+        <button onClick={handleclick} value="5">
+          5
+        </button>
+        <button onClick={handleclick} value="6">
+          6
+        </button>
+        <button onClick={handleclick} value="7">
+          7
+        </button>
+        <button onClick={handleclick} value="8">
+          8
+        </button>
+        <button onClick={handleclick} value="9">
+          9
+        </button>
+        <button onClick={handleclick} value="0">
+          0
+        </button>
+        <button onClick={handleclick} value="-">
+          -
+        </button>
+        <button onClick={handleclick} value="/">
+          /
+        </button>
+        <button onClick={handleclick} value="*">
+          *
+        </button>
+        <button
+          onClick={() => {
+            let evalAns = eval(allBtnClicked).toString();
+            console.log(evalAns);
+            if (evalAns == 'NaN') {
+              setAllbtnClicked('Not a Number');
+            } else {
+              setAllbtnClicked(evalAns);
+            }
+          }}
+          value="="
+        >
+          =
+        </button>
+        <button onClick={handleclick} value=".">
+          .
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
